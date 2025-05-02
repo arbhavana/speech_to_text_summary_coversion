5ae89476da25da702d0828e45eacd59e9c55097b
Bhavana Akula Ravisankar Gari <119329256+arbhavana@users.noreply.github.com>
2025-05-02 22:10:10 +0530

    Update testassembly.js

--- server-backend/testassembly.js
+++ server-backend/testassembly.js
@@ -1,4 +1,7 @@
 const { AssemblyAI } = require("assemblyai");
+require("dotenv").config();
+
+const ASSEMBLY_API_KEY= process.env.ASSEMBLY_API_KEY;
 
 const client = new AssemblyAI({
   apiKey: "f95d887c6fc643528485537c61aa40ab", // Replace if needed
@@ -7,9 +10,9 @@ const client = new AssemblyAI({
 async function testAssembly() {
   try {
     const result = await client.transcripts.list();
-    console.log("✅ AssemblyAI Key is VALID");
+    console.log("AssemblyAI Key is VALID");
   } catch (err) {
-    console.error("❌ AssemblyAI Key is INVALID:", err.message);
+    console.error("AssemblyAI Key is INVALID:", err.message);
   }
 }
 
